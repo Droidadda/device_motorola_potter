7e3ba60ba0da5dd2bfb2ce2b7da2aaeb3958495c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-02-15 23:21:33 -0200

    (DON'T PICK) - Include translations overlay

--- device.mk
+++ device.mk
@@ -21,6 +21,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Translations
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/lineage-translations/overlay/common/
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
